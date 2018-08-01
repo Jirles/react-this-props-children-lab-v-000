388941d13f7b181faaefe2ad9fbe7a90d0e4bfe5
Emily Jirles <emily.jirles@gmail.com>
2018-08-01 20:34:23 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -6,7 +6,11 @@ export default class Invitation extends React.Component {
   render(){
     return(
       <div className='invitation'>
+<<<<<<< HEAD
         <h1>You've been invited!</h1>
+=======
+        <h1>Youve been invited!</h1>
+>>>>>>> 3cb1348c6077a6dc951f4f7e338b80125ffc2028
         {this.props.children}
       </div>
     )
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,5 +1,6 @@
 // Code ThemedDecoration Component Here
 import React from 'react';
+<<<<<<< HEAD
 import PropTypes from 'prop-types';
 
 export default class ThemedDecorations extends React.Component {
@@ -11,6 +12,20 @@ export default class ThemedDecorations extends React.Component {
     return(
       <div>
         {childrenWithNewTheme}
+=======
+import PropTypes from 'prop-types'
+
+export default ThemedDecoration extends React.Component {
+  render(){
+    const childrenWithNewTheme = React.Children.map(child =>  return (this.props.children, child =>
+      return React.cloneElement(child, { theme: this.props.theme })));
+
+    const childrenDivs = React.Children.map(child => return <div className={theme}>{child}</div>)
+
+    return(
+      <div>
+        {childrenDivs}
+>>>>>>> 3cb1348c6077a6dc951f4f7e338b80125ffc2028
       </div>
     )
   }
